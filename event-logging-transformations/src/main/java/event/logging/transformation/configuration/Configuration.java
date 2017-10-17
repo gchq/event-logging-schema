56292ed301d849be4d3f346fd0785b8c03281584
at055612 <at055612@gmail.com>
2017-10-17 16:42:23 +0100

    Get gradle build and java code working

---
+++ event-logging-transformations/src/main/java/event/logging/transformation/configuration/Configuration.java
@@ -0,0 +1,32 @@
+package event.logging.transformation.configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Configuration {
+
+    @JsonProperty(required = true)
+    private String sourceSchemaPath;
+
+    @JsonProperty(required = true)
+    private List<Pipeline> pipelines;
+
+    //no-args constructor for jackson
+    public Configuration() {
+    }
+
+    public Path getRelativeSourceSchemaPath() {
+        return Paths.get(sourceSchemaPath);
+    }
+
+    public Path getSourceSchemaPath(final Path basePath) {
+        return basePath.resolve(sourceSchemaPath);
+    }
+
+    public List<Pipeline> getPipelines() {
+        return pipelines;
+    }
+}
