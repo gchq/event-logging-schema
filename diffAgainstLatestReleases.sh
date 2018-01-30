19db1e34542f3f43b1dcc0891fe1de996583261e
at055612 <at055612@gmail.com>
2018-01-30 13:04:20 +0000

    Change working dir for diff script

--- diffAgainstLatestReleases.sh
+++ diffAgainstLatestReleases.sh
@@ -23,11 +23,12 @@ popd () {
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
     echo -e "${RED}INVALID ARGS!${NC}"
     echo -e "Usage: ${GREEN}$0 workingDir${NC}"
-    echo -e "Where workingDir is normally ${BLUE}event-logging-transformer-main/pipelines/generated${NC}"
+    echo -e "Where workingDir is normally ${BLUE}event-logging-transformer-main/pipelines${NC}"
     exit 1
 fi
 
 workingDir=$1
+pushd ${workingDir}
 
 #Call the github API to git the json for the latest release, then extract the sources jar binary url from it
 #GH_USER_AND_TOKEN decalred in .travis.yml env:/global/:secure
@@ -42,7 +43,8 @@ fi
 echo
 echo -e "${YELLOW}Comparing the latest released schemas from github against the ones just generated${NC}"
 
-#echo "workingDir=${workingDir}"
+echo
+echo -e "Working directory: [${BLUE}${PWD}${NC}]"
 #echo "PWD=${PWD}"
 echo
 echo -e "${GREEN}Latest release URLs${NC}:\n${latestUrls}"
@@ -55,46 +57,47 @@ if [ "${latestUrls}x" = "x" ]; then
     exit 1
 fi
 
-pushd ${workingDir}
 
 mkdir -p downloaded
 rm -rf ./downloaded/*
 
-pushd downloaded
 
 
 #loop over each of the download urls on github
 while read -r url; do
     echo
     echo -e "Downloading schema ${BLUE}${url}${NC}"
+    pushd downloaded
     #Download the released schema from github
     #Need -L to follow re-directs as github will redirect the request
     curl -L -s -O ${url}
+    popd
     #extract the filename part from the url
     downloadedFilename="$(echo "${url}" | sed -ne 's#http.*/\([^/]*\.xsd\)#\1#p')"
+    downloadedFile="./downloaded/${downloadedFilename}"
     #capture the suffix part from the filename, if there is one
     suffix="$(echo "${downloadedFilename}" | sed -ne 's/event-logging-v[0-9]*\(.*\)\.xsd/\1/p')"
 
-    echo -e "Downloaded file: [${GREEN}${downloadedFilename}${NC}]"
+    echo -e "Downloaded file: [${GREEN}${downloadedFile}${NC}]"
     echo -e "Suffix: [${GREEN}${suffix}${NC}]"
 
     #localFile=$(ls ../event-logging-v*${suffix}.xsd)
-    localFile="$(find ../ -maxdepth 1 -regex ".*/event-logging-v[0-9]*${suffix}.xsd")"
+    localFile="$(find ./generated/ -maxdepth 1 -regex ".*/event-logging-v[0-9]*${suffix}.xsd")"
     if [ "x${localFile}" = "x" ]; then 
-        echo -e "${RED}Warning${NC}: no local file found to match downloaded file ${BLUE}${downloadedFilename}${NC}"
+        echo -e "${RED}Warning${NC}: no local file found to match downloaded file ${BLUE}${downloadedFile}${NC}"
     else
         echo -e "Local file: [${GREEN}${localFile}${NC}]"
 
-        echo -e "Diffing ${BLUE}${localFile}${NC} against ${BLUE}${downloadedFilename}${NC}"
-        diffFile="${downloadedFilename}.diff"
+        echo -e "Diffing ${BLUE}${localFile}${NC} against ${BLUE}${downloadedFile}${NC}"
+        diffFile="${downloadedFile}.diff"
         #OR with true to stop the exit code from diff stopping the script
-        diff ${localFile} ${downloadedFilename} > ${diffFile} || true
+        diff ${localFile} ${downloadedFile} > ${diffFile} || true
         if [ $(cat ${diffFile} | wc -l) -gt 0 ]; then
-            diff ${localFile} ${downloadedFilename} || true
-            echo -e "\n${RED}Warning${NC}: Local schema ${BLUE}${localFile}${NC} differs from ${BLUE}${downloadedFilename}${NC}"
+            diff ${localFile} ${downloadedFile} || true
+            echo -e "\n${RED}Warning${NC}: Local schema ${BLUE}${localFile}${NC} differs from ${BLUE}${downloadedFile}${NC}"
             echo -e "(see changes in $PWD/${diffFile})${NC}"
         else 
-            echo -e "\n${GREEN}Local schema ${BLUE}${localFile}${NC} is identical to ${BLUE}${downloadedFilename}${NC}"
+            echo -e "\n${GREEN}Local schema ${BLUE}${localFile}${NC} is identical to ${BLUE}${downloadedFile}${NC}"
         fi
     fi
 done <<< "$latestUrls"
