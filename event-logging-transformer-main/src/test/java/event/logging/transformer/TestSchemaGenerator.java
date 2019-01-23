6b63efc5ebf060717c06c8c1daec0559b1d1df0b
at055612 <at055612@gmail.com>
2019-01-23 12:23:53 +0000

    Add new test, add diffUtils lib, add filename arg to validation script

--- event-logging-transformer-main/src/test/java/event/logging/transformer/TestSchemaGenerator.java
+++ event-logging-transformer-main/src/test/java/event/logging/transformer/TestSchemaGenerator.java
@@ -1,5 +1,9 @@
 package event.logging.transformer;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Patch;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,38 +14,109 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class TestSchemaGenerator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestRealPipelines.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestSchemaGenerator.class);
+    private static final Path SOURCE_SCHEMA = Paths.get("../event-logging.xsd");
+    private static final Path TEST_DATA_ROOT_DIR = Paths.get("src/test/resources/test-data");
+    private static final Path PIPELINES_RELATIVE_PATH = Paths.get("pipelines");
+    private static final Path GENERATED_RELATIVE_PATH = Paths.get("pipelines/generated");
+    private static final Path UNCHANGED_FILE = GENERATED_RELATIVE_PATH.resolve("event-logging-v3.xsd");
 
-    private static final Path GENERATED_PATH = Paths.get("./pipelines/generated");
+    private static final String TEST_01 = "test-01";
+    private static final List<String> TEST_CASE_NAMES = Arrays.asList(
+            TEST_01
+    );
 
     @Before
-    public void setup() throws IOException {
+    public void setup() {
 
-        if (Files.exists(GENERATED_PATH)) {
-            Assertions.assertThat(GENERATED_PATH).isDirectory();
-            SchemaGenerator.emptyDirectory(GENERATED_PATH);
-        }
+        // Clean out all the generated content
+        TEST_CASE_NAMES.forEach(testCaseName -> {
+           final Path testCaseDir = getTestCaseDir(testCaseName);
+           final Path generatedDir = testCaseDir.resolve(GENERATED_RELATIVE_PATH);
+
+            if (Files.exists(generatedDir)) {
+                Assertions.assertThat(generatedDir).isDirectory();
+                try {
+                    SchemaGenerator.emptyDirectory(generatedDir);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     @Test
-    public void testMain() throws IOException {
+    public void test01_versionChange() throws IOException {
+
+        runTest(TEST_01);
+    }
+
+    private void runTest(final String name) throws IOException {
+
+        final Path testCaseDir = getTestCaseDir(name);
+        final Path pipelinesDir = testCaseDir.resolve(PIPELINES_RELATIVE_PATH);
+        final Path generatedDir = testCaseDir.resolve(GENERATED_RELATIVE_PATH);
+
         SchemaGenerator.main(new String[]{
-                "pipelines",
-                "../event-logging.xsd"});
+                pipelinesDir.toString(),
+                SOURCE_SCHEMA.toString()});
 
-        //two pipelines in config so should result in 2 files
-        Assertions.assertThat(Files.list(GENERATED_PATH).count())
-                .isEqualTo(2);
+        // Our test should have generated an unchanged file plus at least one more
+        Assertions.assertThat(Files.list(generatedDir).count())
+                .isGreaterThanOrEqualTo(2);
 
-        Files.list(GENERATED_PATH)
+        final Path unchangedFile = testCaseDir.resolve(UNCHANGED_FILE);
+
+        Files.list(generatedDir)
+                .filter(file -> ! file.getFileName().equals(unchangedFile.getFileName()))
                 .forEach(file -> {
                     Assertions.assertThat(file)
                             .isRegularFile();
-                    Assertions.assertThat(file.getFileName().toString())
-                            .endsWith(".xsd");
+
+                    diffFileAgainstSourceSchema(unchangedFile, file);
                 });
+    }
+
+    private void diffFileAgainstSourceSchema(final Path originalFile, final Path revisedFile) {
+
+        //build simple lists of the lines of the two testfiles
+        final List<String> original = getFileLines(originalFile);
+        final List<String> revised = getFileLines(revisedFile);
+
+        //compute the patch: this is the diffutils part
+        final Patch<String> patch;
+        try {
+            patch = DiffUtils.diff(original, revised);
+        } catch (DiffException e) {
+            throw new RuntimeException("Error diffing files");
+        }
+
+        LOGGER.info("Comparing {} to {}", originalFile.toString(), revisedFile.toString());
+        //simple output the computed patch to console
+        for (AbstractDelta<String> delta : patch.getDeltas()) {
+            System.out.println(String.format("Original [line %s]", delta.getSource().getPosition()));
+            delta.getSource().getLines().forEach(System.out::println);
+            System.out.println(String.format("Revised [line %s]", delta.getTarget().getPosition()));
+            delta.getTarget().getLines().forEach(System.out::println);
+        }
+    }
+
+    private List<String> getFileLines(final Path file) {
+        try {
+            return Files.readAllLines(file);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Error reading file %s", file));
+        }
+    }
+
+    private Path getTestCaseDir(final String testCaseName) {
+        Objects.requireNonNull(testCaseName);
+        return TEST_DATA_ROOT_DIR.resolve(testCaseName);
     }
 }
